a23c92f21b89d0ea1bb857f4aed1c55d187d19ca
Moon-YJ <tunjeong@gmail.com>
2023-12-26 10:29:29 +0900

    useQuery로 비동기 데이터 반환

---
+++ src/UserInfo.jsx
@@ -0,0 +1,32 @@
+import { useQuery } from "@tanstack/react-query";
+
+export default function UserInfo() {
+  const fetchUser = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    return await response.json();
+  };
+
+  // useQuery(queryKey, fetch함수);
+  // queryKey: react-query로 관리할 각 비동기 데이터 구분 용도의 키
+  /*
+    data: 실제 반환 데이터
+    isError, isSuccess: 데이터 반환 실패, 성공 여부
+    isLoading: pending 유무
+    isStale: 최신 데이터 유무 (false이면 최신 상태, true이면 옛날 상태) <-> isFresh
+    refetch: 비동기 데이터 강제 재요청 함수
+  */
+  const { data, isLoading, isSuccess } = useQuery({
+    queryKey: ["users"],
+    queryFn: fetchUser,
+  });
+
+  // useQuery 이용시 useState를 통한 지역 State 생성, useEffect를 통한 fetching함수 호출, useCallback을 통한 메모이제이션 처리 => 이 모든과정을 안해도 한번에 useQuery가 위의 모든 작업을 대신 처리해줌
+
+  return (
+    <div className="UserInfo">
+      <h1>User Information</h1>
+      {isLoading && <p>loading...</p>}
+      {isSuccess && data.map((user) => <h2 key={user.id}>{user.name}</h2>)}
+    </div>
+  );
+}
