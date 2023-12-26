85072d6245ad89e372ddf424a7252939c0f4bade
Moon-YJ <tunjeong@gmail.com>
2023-12-26 11:23:33 +0900

    서버 데이터가 활용되지 않는 컴포넌트에서는 자동적으로 inactive 상태로 비활성화

--- src/Main.jsx
+++ src/Main.jsx
@@ -2,6 +2,7 @@ export default function Main() {
   return (
     <div className="Main">
       <h1>Main</h1>
+      {/* react-query로 관리되지만 서버 데이터가 활용되지않는 Main 컴포넌트로 이동시 자동으로 해당 데이터는 inactive 상태가 됨, 이때부터 기본 설정된 캐시 타임이 카운트 되면서 캐시타임이 끝나면 자동으로 해당 데이터는 아예 제거됨  */}
     </div>
   );
 }
